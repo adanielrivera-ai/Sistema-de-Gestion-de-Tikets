2a819a1e62dd0100fba30b08ac957e1e1dec4d9a
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-21 11:14:57 -0500

    feat: crear clase RutaDAO con atributo private static final

---
+++ src/sistemagestiontikets/dao/RutaDAO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagestiontikets.dao;
+
+import sistemagestiontikets.model.Ruta;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Camilo
+ */
+public class RutaDAO {
+    private static final String ARCHIVO = "rutas.txt";
+}
