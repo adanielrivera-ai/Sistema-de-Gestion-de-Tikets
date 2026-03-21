dda5435f38e712fd18f699f5f4557a3c379977f2
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-21 11:09:04 -0500

    feat: crear la clase VehiculoDAO con atributos private static final

---
+++ src/sistemagestiontikets/dao/VehiculoDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagestiontikets.dao;
+
+import sistemagestiontikets.model.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Camilo
+ */
+public class VehiculoDAO {
+    private static final String BUSETA   = "busetas.txt";
+    private static final String MICROBUS = "microbuses.txt";
+    private static final String BUS      = "buses.txt";
+}
