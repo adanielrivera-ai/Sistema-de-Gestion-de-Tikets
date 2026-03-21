0a4dff1649436e0519159f6662edb2f940189de6
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-21 10:38:25 -0500

    feat: Crear clase Vehiculo con atributos

---
+++ src/sistemagestiontikets/model/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagestiontikets.model;
+
+/**
+ *
+ * @author Camilo
+ */
+public abstract class Vehiculo {
+    protected String placa;
+    protected Ruta   ruta;
+    protected int    capacidadMaxima;
+    protected int    contadorPasajeros;
+    protected double tarifaBase;
+
+}
