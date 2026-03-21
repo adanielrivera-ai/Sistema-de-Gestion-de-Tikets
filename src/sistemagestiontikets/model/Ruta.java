b12c6ca22435e0f1f4acf2102cea06234494041e
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-21 10:25:12 -0500

    feat: Crear la clase Ruta con atributos

---
+++ src/sistemagestiontikets/model/Ruta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagestiontikets.model;
+
+/**
+ *
+ * @author Camilo
+ */
+public class Ruta {
+    private String codigo;
+    private String origen;
+    private String destino;
+    private double distanciaKm;
+    private int    tiempoEstimadoMin;
+
+}
