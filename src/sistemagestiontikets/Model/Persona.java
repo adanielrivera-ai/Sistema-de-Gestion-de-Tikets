c426369bc1bf90cdae8fa806a87051d500cf7109
Kamilo0066 <jkamilofernandez@unicesar.edu.co>
2026-03-20 21:58:05 -0500

    feat: crear clase Persona con atributos

--- src/sistemagestiontikets/Model/Persona.java
+++ src/sistemagestiontikets/Model/Persona.java
@@ -2,12 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package sistemagestiontikets.Model;
+package sistemagestiontikets.model;
 
 /**
  *
  * @author JAVIER FERNANDEZ
  */
-public abstract class Persona {
-    
+public abstract class Persona implements Imprimible {
+    protected String cedula;
+    protected String nombre;
+
+    public Persona(String cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public String getCedula() { return cedula; }
+    public String getNombre() { return nombre; }
+
+    @Override
+    public abstract void imprimirDetalle();
 }
