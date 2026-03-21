1ebe14e2476074b2566ea919f4a10fe0f829cb45
CamiloMancilla <candresmancilla@unicesar.edu.co>
2026-03-21 11:20:36 -0500

    feat: crear la clase VehiculoService con atributos

---
+++ src/sistemagestiontikets/service/VehiculoService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagestiontikets.service;
+
+import sistemagestiontikets.dao.RutaDAO;
+import sistemagestiontikets.dao.VehiculoDAO;
+import sistemagestiontikets.model.*;
+import java.util.*;
+
+/**
+ *
+ * @author Camilo
+ */
+public class VehiculoService {
+    private List<Vehiculo> vehiculos;
+    private List<Ruta> rutas;
+    private VehiculoDAO vehiculoDAO;
+    private RutaDAO rutaDAO;    
+}
